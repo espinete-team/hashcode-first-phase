acdd61e897ee2c374069cf692bba5604e4ab27c0
RD91038 <rdecarva@BCN-3G7RGH2.usersad.everis.int>
2019-02-28 20:42:12 +0100

    Implementing ReadInput.

--- src/main/java/slideshow/ReadInput.java
+++ src/main/java/slideshow/ReadInput.java
@@ -3,21 +3,60 @@ package slideshow;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ReadInput {
+
+	public static final String A_EXAMPLE = "src/main/resources/a_example.txt";
+	public static final String LOVELY_LANDSCAPES = "src/main/resources/b_lovely_landscapes.txt";
+	public static final String MEMORABLE_MOMENTS = "src/main/resources/c_memorable_moments.txt";
+	public static final String PET_PICTURES = "src/main/resources/d_pet_pictures.txt";
+	public static final String SHINY_SELFIES = "src/main/resources/e_shiny_selfies.txt";
 	
-	private final String A_EXAMPLE = "src/main/resources/a_example.txt";
+	private String input;
+	
+	public ReadInput(String input) {
+		this.input = input;
+	}
 
 	public String getStringFromFile() {
 		String content = "";
 
 		try {
-			content = new String(Files.readAllBytes(Paths.get(A_EXAMPLE)));
+			content = new String(Files.readAllBytes(Paths.get(input)));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
 		return content;
+	}
+
+	public int getNumberOfPhotos(String content) {
+		String number = content.split("\n")[0];
+
+		return Integer.valueOf(number);
+	}
+
+	public List<Photo> getPhotosFromString(String allContent) {
+		String[] content = allContent.split("\n");
+		List<Photo> photos = new ArrayList<Photo>();
+
+		for (int i = 1; i < content.length; i++) {
+			String word[] = content[i].split(" ");
+
+			Photo photo = new Photo();
+			photo.setId(i - 1);
+			photo.setOrientation(word[0]);
+
+			for (int j = 2; j < Integer.valueOf(word[1])+2; j++) {
+				photo.getTags().add(word[j]);
+			}
+
+			photos.add(photo);
+		}
+
+		return photos;
 	}
 
 }
--- src/test/java/slideshow/ReadInputTest.java
+++ src/test/java/slideshow/ReadInputTest.java
@@ -1,15 +1,55 @@
 package slideshow;
 
+import java.util.List;
+
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import static slideshow.ReadInput.A_EXAMPLE;
 
 public class ReadInputTest {
 	
+	ReadInput readInput;
+	
+	@Before
+	public void setUp() {
+		readInput = new ReadInput(A_EXAMPLE);
+	}
+	
 	@Test
 	public void testWhenGetInputReturnsString() {
-		ReadInput readInput = new ReadInput();
 		Assert.assertEquals(String.class, readInput.getStringFromFile().getClass());
+	}
+	
+	@Test
+	public void testWhenGetFirstLineReturnsInteger() {
+		int firstLine = readInput.getNumberOfPhotos(readInput.getStringFromFile());
+		Assert.assertEquals(4, firstLine);
+	}
+	
+	@Test
+	public void testWhenStringisPassedReturnsPhotoCollection() {
+		List<Photo> photos = readInput.getPhotosFromString(readInput.getStringFromFile());
+		
+		Assert.assertEquals(4, photos.size());
+		
+		Assert.assertEquals("H", photos.get(0).getOrientation());
+		Assert.assertEquals("cat", photos.get(0).getTags().get(0));
+		Assert.assertEquals("beach", photos.get(0).getTags().get(1));
+		Assert.assertEquals("sun", photos.get(0).getTags().get(2));
+		
+		Assert.assertEquals("V", photos.get(1).getOrientation());
+		Assert.assertEquals("selfie", photos.get(1).getTags().get(0));
+		Assert.assertEquals("smile", photos.get(1).getTags().get(1));
+		
+		Assert.assertEquals("V", photos.get(2).getOrientation());
+		Assert.assertEquals("garden", photos.get(2).getTags().get(0));
+		Assert.assertEquals("selfie", photos.get(2).getTags().get(1));
+		
+		Assert.assertEquals("H", photos.get(3).getOrientation());
+		Assert.assertEquals("garden", photos.get(3).getTags().get(0));
+		Assert.assertEquals("cat", photos.get(3).getTags().get(1));
 	}
 	
 }
